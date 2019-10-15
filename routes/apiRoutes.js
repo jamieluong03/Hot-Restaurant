a55b7b01ce18c8d36490d985308fb7a1061483a0
Jamie Luong <jamieluong03@gmail.com>
2019-10-15 14:31:33 -0700

    updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -22,7 +22,7 @@ module.exports = function(app) {
   app.get("/api/tables", function(req, res) {
     res.json(tableData);
   });
-  app.get("/api/reserve", function(req, res){
+  app.get("/api/waitlist", function(req, res){
     res.json(waitListData);
   });
 
@@ -34,7 +34,19 @@ module.exports = function(app) {
   // (ex. User fills out a reservation request... this data is then sent to the server...
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
-  app.post
+  app.post("/api/tables", function(req, res){
+    var newCustomer = req.body;
+    tableData.push(newCustomer);
+    res.json(newCustomer);
+
+    if (tableData.length > 5){
+      app.post("/api/waitlist", function(req, res){
+        var newWait = req.body;
+        waitListData.push(newWait);
+        res.json(newWait);
+      });
+    };
+  });
 
   // ---------------------------------------------------------------------------
   // I added this below code so you could clear out the table while working with the functionality.
